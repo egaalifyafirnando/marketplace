5940397592984e0b886517a89b843c6126833dee
egaalfy <egaalifyaf@gmail.com>
2022-03-18 22:07:56 +0700

    slicing page html to blade template

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="pt-4 pb-2">&copy;2022 | Ega Alifya Firnando.</p>
+            </div>
+        </div>
+    </div>
+</footer>
